929df54543367f6918fcaa75efa7c07b3c225494
stevenmadams <stevenadams@me.com>
2016-09-15 16:26:09 -0600

    Added RESOURCE and TAG objects to BootPrepJPA->src->entities

---
+++ BootPrepJPA/src/entities/Resource.java
@@ -0,0 +1,58 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+	public class Resource {
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+		private String name;
+		private String description;
+		private String url;
+		private String video;
+		private String photo;
+		
+		
+		
+//GET AND SET---------------------------------------------------
+
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public String getVideo() {
+			return video;
+		}
+		public void setVideo(String video) {
+			this.video = video;
+		}
+		public String getPhoto() {
+			return photo;
+		}
+		public void setPhoto(String photo) {
+			this.photo = photo;
+		}
+		public int getId() {
+			return id;
+		}
+
+//ADD AND REMOVE METHODS---------------------------------------------------
+}
---
+++ BootPrepJPA/src/entities/Tag.java
@@ -0,0 +1,14 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Tag {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+}
