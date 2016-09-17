bc004ca3a239d156f27dd8621d34032474bb4efd
Josiah Moye <jmoye@neo.rr.com>
2016-09-17 15:03:13 -0600

    Resource list all

--- BootPrepWeb/src/controllers/ResourceController.java
+++ BootPrepWeb/src/controllers/ResourceController.java
@@ -1,16 +1,30 @@
 package controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
 
 import dao.BootPrepDAO;
+import entities.Resource;
 
 @Controller
+@SessionAttributes({"userId","auth"})
 public class ResourceController {
 
 	@Autowired
 	private BootPrepDAO dao;
 	
+	@RequestMapping(path="resourcelist.do")
+	public ModelAndView listAllResources() {
+		List<Resource> resources = new ArrayList<>();
+		resources = dao.getAllResources();
+		return new ModelAndView("resourcelist.jsp", "resources", resources);
+	}
 	
 	
 }
---
+++ BootPrepWeb/src/helpers/CustomTag.java
@@ -0,0 +1,10 @@
+package helpers;
+
+import java.util.List;
+
+public class CustomTag {
+	public static boolean contains(List list, Object o) {
+		return list.contains(o);
+	}
+	
+}
